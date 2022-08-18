0548e2eb62b058e80db709a84dbca16f0bfebf39
Víctor <victorsobrinos@gmail.com>
2022-08-18 21:27:40 +0200

    linter ts.standard installed

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,6 +1,9 @@
+import { DiaryEntry } from '../type'
 import diaryData from './diaries.json'
 //"resolveJsonModule": true --> added in the config file to be able to read json file
 
-export const getEntries = () => diaryData
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[] //<-- Aserción de tipos (evitar siempre que se pueda)
+//DiaryEntry[] = Array<DiaryEntry>
+export const getEntries = (): DiaryEntry[] => diaries
 
 export const addEntry = () => null
