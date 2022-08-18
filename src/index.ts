8e2ee417505099fc6ea57da6752bbc62a395a5fe
Víctor <victorsobrinos@gmail.com>
2022-08-18 19:52:56 +0200

    Server created and dependencies installed

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+app.use(express.json()) //middleware to transform req.body to json file
+
+const PORT = 3000
+
+app.get('/ping', (_req, res) => { //_ typeScript lo ignora
+    console.log('HEllo!')
+    res.send('pong')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
