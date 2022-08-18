8d6bf2ffa8527c9422cfd74f81390345a77b5493
Víctor <victorsobrinos@gmail.com>
2022-08-18 20:44:29 +0200

    new types and an interfece created

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,6 @@
+import diaryData from './diaries.json'
+//"resolveJsonModule": true --> added in the config file to be able to read json file
+
+export const getEntries = () => diaryData
+
+export const addEntry = () => null
