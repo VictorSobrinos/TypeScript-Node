3550226a0ff49ba6bee88302d6c11a6bb1decab5
Víctor <victorsobrinos@gmail.com>
2022-08-19 19:54:35 +0200

    new type without comments

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,9 +1,11 @@
-import { DiaryEntry } from '../type'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../type'
 import diaryData from './diaries.json'
 //"resolveJsonModule": true --> added in the config file to be able to read json file
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[] //<-- Aserción de tipos (evitar siempre que se pueda)
 //DiaryEntry[] = Array<DiaryEntry>
 export const getEntries = (): DiaryEntry[] => diaries
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => diaries
 
 export const addEntry = () => null
